c3ea7516e29c880b90d2d6b6f39c5749e3f1fcd6
SethaReno <51383197+SethaReno@users.noreply.github.com>
2020-03-20 17:55:00 -0700

    layed out shipping form fields

--- src/components/information/shipping.js
+++ src/components/information/shipping.js
@@ -21,10 +21,10 @@ class Shipping extends Component {
     }
     render() {
         return (
-            <div className='shipping-form'>
-                <PageTitle className='shipping-form__page-title' title='Shipping Address'/>
+            <div className='shipping'>
+                <PageTitle className='shipping__page-title' title='Shipping Address'/>
                 <i class="fas fa-search"></i>
-                <ShippingForm onSubmit={this.onSubmit} className='shipping-form__form'/>
+                <ShippingForm onSubmit={this.onSubmit} className='shipping__form'/>
             </div> 
         )
     }
